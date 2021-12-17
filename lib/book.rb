6ad9adeabd1f88b5c3d6b3fe91e70e299cb9d45f
Marcus <marcus.shimotsu@gmail.com>
2021-12-16 19:17:04 -0600

    commit tree 865851531c52f87ad17af4a1a789e9207e502f2d

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,13 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :genre
+    attr_accessor :page_count
+    def initialize(title)
+        @title = title
+    end
 
-
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
